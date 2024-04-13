3aa0e1b23f0eb845f643ea1d8123b457ec940b72
ra-kesh <iamrakeshkumar@pm.me>
2024-04-14 00:38:57 +0530

    feat:added generate image

--- app/(dashboard)/generate-image/page.tsx
+++ app/(dashboard)/generate-image/page.tsx
@@ -1,35 +1,15 @@
 import React from "react"
-import Image from "next/image"
 
 import { Card, CardContent, CardHeader } from "@/components/ui/card"
 
 import HistoryTable from "./_component/HistoryTable"
 import ImageGenerationForm from "./_component/ImageGenerationForm"
-import imagegeneration from "/public/image_generation_v1.png"
+import Preview from "./_component/Preview"
 
 const GenerateImage = () => {
   return (
     <div className="grid grid-cols-1 gap-x-4 lg:grid-cols-8">
-      <Card className="bg-inherit  lg:col-span-5">
-        <CardHeader className="px-0 text-2xl font-medium">Preview</CardHeader>
-        <CardContent className="grid grid-cols-1 gap-4 rounded-xl bg-card p-6 lg:grid-cols-2 xl:grid-cols-3">
-          <Image src={imagegeneration} alt="generated image" className="lg:col-span-1" />
-
-          <Image src={imagegeneration} alt="generated image" className="lg:col-span-1" />
-
-          <Image src={imagegeneration} alt="generated image" className="lg:col-span-1" />
-          <Image src={imagegeneration} alt="generated image" className="lg:col-span-1" />
-
-          <Image src={imagegeneration} alt="generated image" className="lg:col-span-1" />
-
-          <Image src={imagegeneration} alt="generated image" className="lg:col-span-1" />
-          <Image src={imagegeneration} alt="generated image" className="lg:col-span-1" />
-
-          <Image src={imagegeneration} alt="generated image" className="lg:col-span-1" />
-
-          <Image src={imagegeneration} alt="generated image" className="lg:col-span-1" />
-        </CardContent>
-      </Card>
+      <Preview />
       <Card className=" bg-inherit lg:col-span-3">
         <CardHeader className="px-0 text-2xl font-medium">AI Image Generation</CardHeader>
         <CardContent className="gap-4 rounded-xl bg-form p-6 ">
@@ -42,3 +22,43 @@ const GenerateImage = () => {
 }
 
 export default GenerateImage
+
+export const images = [
+  {
+    id: 20991,
+    url: "/image_gen_9_v1.png",
+  },
+  {
+    id: 209,
+    url: "/image_generation_2_v1.png",
+  },
+  {
+    id: 209918,
+    url: "/image_generation_3_v1.png",
+  },
+  {
+    id: 20840,
+    url: "/image_generation_4_v1.png",
+  },
+  {
+    id: 2840,
+    url: "/image_generation_5_v1.png",
+  },
+  {
+    id: 2090,
+    url: "/image_generation_6_v1.png",
+  },
+  {
+    id: 1840,
+    url: "/image_generation_3_v1.png",
+  },
+
+  {
+    id: 91840,
+    url: "/image_generation_2_v1.png",
+  },
+  {
+    id: 240,
+    url: "/image_generation_v1.png",
+  },
+]
