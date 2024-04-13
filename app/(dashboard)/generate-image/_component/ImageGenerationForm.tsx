4a5978da448ce297040e75c3e663d15803ce5ea9
ra-kesh <iamrakeshkumar@pm.me>
2024-04-13 13:37:54 +0530

    feat:initial image generation form

---
+++ app/(dashboard)/generate-image/_component/ImageGenerationForm.tsx
@@ -0,0 +1,148 @@
+"use client"
+
+import React from "react"
+import { useForm } from "react-hook-form"
+
+import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { Slider } from "@/components/ui/slider"
+import { Textarea } from "@/components/ui/textarea"
+
+const ImageGenerationForm = () => {
+  const form = useForm()
+  return (
+    <div className="grid grid-cols-subgrid">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(() => {})} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem className="col-span-2">
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="shadcn" {...field} className="border-none" />
+                </FormControl>
+                <FormDescription>This is your public display name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem className="lg:col-span-2">
+                <FormLabel>Sampling steps</FormLabel>
+                <FormControl>
+                  <Slider defaultValue={[33]} max={100} step={1} onChange={field.onChange} />
+                </FormControl>
+                <FormDescription>This is your public display name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="type"
+            render={({ field }) => (
+              <FormItem className="space-y-3">
+                <FormLabel>Notify me about...</FormLabel>
+                <FormControl>
+                  <RadioGroup
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    className="flex flex-col space-y-1"
+                  >
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="all" />
+                      </FormControl>
+                      <FormLabel className="font-normal">All new messages</FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="mentions" />
+                      </FormControl>
+                      <FormLabel className="font-normal">Direct messages and mentions</FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="none" />
+                      </FormControl>
+                      <FormLabel className="font-normal">Nothing</FormLabel>
+                    </FormItem>
+                  </RadioGroup>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a verified email to display" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="m@example.com">m@example.com</SelectItem>
+                    <SelectItem value="m@google.com">m@google.com</SelectItem>
+                    <SelectItem value="m@support.com">m@support.com</SelectItem>
+                  </SelectContent>
+                </Select>
+                {/* <FormDescription>
+                You can manage email addresses in your{" "}
+                <Link href="/examples/forms">email settings</Link>.
+              </FormDescription> */}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex flex-row">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem className="w-1/2">
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>This is your public display name.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem className="w-1/2">
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>This is your public display name.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          {/* <Button type="submit">Submit</Button> */}
+        </form>
+      </Form>
+    </div>
+  )
+}
+
+export default ImageGenerationForm
--- app/(dashboard)/generate-image/page.tsx
+++ app/(dashboard)/generate-image/page.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React from "react"
+import Image from "next/image"
+
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
+
+import ImageGenerationForm from "./_component/ImageGenerationForm"
+import imagegeneration from "/public/image_generation_v1.png"
 
 const GenerateImage = () => {
   return (
-    <div>GenerateImage</div>
+    <div className="grid grid-cols-1 gap-x-4 lg:grid-cols-8">
+      <Card className="bg-inherit  lg:col-span-5">
+        <CardHeader className="px-0 text-2xl font-medium">Preview</CardHeader>
+        <CardContent className="grid grid-cols-1 gap-4 rounded-xl bg-card p-6 lg:grid-cols-2 xl:grid-cols-3">
+          <Image src={imagegeneration} alt="generated image" className="lg:col-span-1" />
+
+          <Image src={imagegeneration} alt="generated image" className="lg:col-span-1" />
+
+          <Image src={imagegeneration} alt="generated image" className="lg:col-span-1" />
+          <Image src={imagegeneration} alt="generated image" className="lg:col-span-1" />
+
+          <Image src={imagegeneration} alt="generated image" className="lg:col-span-1" />
+
+          <Image src={imagegeneration} alt="generated image" className="lg:col-span-1" />
+        </CardContent>
+      </Card>
+      <Card className=" bg-inherit lg:col-span-3">
+        <CardHeader className="px-0 text-2xl font-medium">AI Image Generation</CardHeader>
+        <CardContent className=" gap-4 rounded-xl bg-card p-6 ">
+          <ImageGenerationForm />
+        </CardContent>
+      </Card>
+    </div>
   )
 }
 
