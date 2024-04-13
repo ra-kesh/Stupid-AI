1df53ea31605f335ee332cd73a1d81555a0b48fb
ra-kesh <iamrakeshkumar@pm.me>
2024-04-14 00:35:47 +0530

    feat:added carousel

---
+++ app/(dashboard)/generate-image/_component/Carousel.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
+
+export const CarouselComponent = ({ images, setSelectedImage }: { images: any[]; setSelectedImage: any }) => {
+  return (
+    <Carousel className="mx-auto max-w-md ">
+      <CarouselContent className="-ml-1">
+        {images.map((image, index) => {
+          return (
+            <CarouselItem
+              key={index}
+              className="pl-1 md:basis-1/2 lg:basis-1/4 xl:basis-1/5"
+              onClick={() => setSelectedImage(image)}
+            >
+              <div className="h-full p-1">
+                <img src={image.url} alt="generated image" className=" rounded-xs size-full" />
+              </div>
+            </CarouselItem>
+          )
+        })}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
