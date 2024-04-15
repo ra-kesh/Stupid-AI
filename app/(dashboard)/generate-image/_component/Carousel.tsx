f001997836d5118759ecd3c3bfccf907c2fdb841
ra-kesh <iamrakeshkumar@pm.me>
2024-04-15 15:29:13 +0530

    style:4xl-update

--- app/(dashboard)/generate-image/_component/Carousel.tsx
+++ app/(dashboard)/generate-image/_component/Carousel.tsx
@@ -4,16 +4,16 @@ import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious
 
 export const CarouselComponent = ({ images, setSelectedImage }: { images: any[]; setSelectedImage: any }) => {
   return (
-    <Carousel className="mx-auto max-w-md ">
+    <Carousel className="mx-auto max-w-md 4xl:max-w-4xl">
       <CarouselContent className="-ml-1">
         {images.map((image, index) => {
           return (
             <CarouselItem
               key={index}
-              className="pl-1 md:basis-1/2 lg:basis-1/4 xl:basis-1/5"
+              className="pl-1 md:basis-1/2 lg:basis-1/4 xl:basis-1/6"
               onClick={() => setSelectedImage(image)}
             >
-              <div className="h-full p-1">
+              <div className="h-full p-1 ">
                 <img src={image.url} alt="generated image" className=" rounded-xs size-full" />
               </div>
             </CarouselItem>
