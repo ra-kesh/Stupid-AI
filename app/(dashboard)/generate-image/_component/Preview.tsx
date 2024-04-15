d3466ae5f48503ffcb9620a96292827a50797c81
ra-kesh <iamrakeshkumar@pm.me>
2024-04-15 21:12:16 +0530

    style:fin-preview-dashboard

--- app/(dashboard)/generate-image/_component/Preview.tsx
+++ app/(dashboard)/generate-image/_component/Preview.tsx
@@ -26,8 +26,8 @@ const Preview = () => {
           <div className="space-y-2">
             <div className="mx-auto rounded-t-lg bg-form p-6 4xl:rounded-t-3xl 4xl:p-10">
               <img src={selectedImage.url} alt="selected image " className="w-full rounded-t-lg" />
-              <div className="flex w-full justify-center 4xl:pt-10">
-                <Button className="m-auto bg-gradient-to-bl from-[#7f04e3] to-[#ff006b] 4xl:h-20 4xl:rounded-3xl 4xl:px-24 4xl:text-2xl">
+              <div className="flex w-full justify-center lg:pt-6 4xl:pt-10">
+                <Button className="4xl:h-18 m-auto bg-gradient-to-bl from-[#7f04e3] to-[#ff006b] lg:rounded-xl lg:px-8  2xl:h-12 2xl:px-16 4xl:rounded-3xl 4xl:px-24 4xl:text-2xl">
                   Download
                 </Button>
               </div>
