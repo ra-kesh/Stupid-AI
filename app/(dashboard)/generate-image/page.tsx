6e3b8648e922424b632a772aee9aac588fe70630
ra-kesh <iamrakeshkumar@pm.me>
2024-04-15 15:28:53 +0530

    style:4xl-update

--- app/(dashboard)/generate-image/page.tsx
+++ app/(dashboard)/generate-image/page.tsx
@@ -11,8 +11,8 @@ const GenerateImage = () => {
     <div className="grid grid-cols-1 gap-x-4 lg:grid-cols-8">
       <Preview />
       <Card className=" bg-inherit lg:col-span-3">
-        <CardHeader className="px-0 text-2xl font-medium">AI Image Generation</CardHeader>
-        <CardContent className="gap-4 rounded-xl bg-form p-6 ">
+        <CardHeader className="px-0 text-2xl font-medium 2xl:text-3xl 4xl:text-4xl">AI Image Generation</CardHeader>
+        <CardContent className="gap-4 rounded-xl bg-form p-6 4xl:rounded-3xl 4xl:p-10">
           <ImageGenerationForm />
         </CardContent>
       </Card>
