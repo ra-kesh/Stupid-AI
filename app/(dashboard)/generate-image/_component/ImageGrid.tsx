61d5952e90857adc9b366c0e81050758271d99c4
ra-kesh <iamrakeshkumar@pm.me>
2024-04-15 15:29:50 +0530

    style:4xl-update

--- app/(dashboard)/generate-image/_component/ImageGrid.tsx
+++ app/(dashboard)/generate-image/_component/ImageGrid.tsx
@@ -1,8 +1,6 @@
 import React from "react"
-import Image from "next/image"
 
 import { Button } from "@/components/ui/button"
-import { CardContent } from "@/components/ui/card"
 
 const ImageGrid = ({
   images,
@@ -15,13 +13,13 @@ const ImageGrid = ({
   setSelectedImage: any
 }) => {
   return (
-    <div className="grid grid-cols-1 gap-4 rounded-xl bg-form p-6 lg:grid-cols-2 xl:grid-cols-3">
+    <div className="grid grid-cols-1 gap-4 rounded-xl bg-form p-6 lg:grid-cols-2 xl:grid-cols-3 4xl:rounded-3xl 4xl:p-10">
       {images?.map((image: any) => (
         <div className="group relative lg:col-span-1" key={image.id}>
           <img src={image.url} alt="generated image" className="size-full rounded-xl" />
           <div className="absolute left-0 top-0 flex size-full flex-col items-center justify-center opacity-0 transition-opacity duration-300 group-hover:opacity-100">
             <Button
-              className="w-2/3 bg-gradient-to-bl from-[#7f04e3] to-[#ff006b]"
+              className="w-2/3 bg-gradient-to-bl from-[#7f04e3] to-[#ff006b] 4xl:h-16 4xl:rounded-3xl 4xl:text-2xl"
               onClick={() => setSelectedImage(image)}
             >
               View
