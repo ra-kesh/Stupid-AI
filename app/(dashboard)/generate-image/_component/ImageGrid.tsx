48b714395e5a74454c34e618947079c687c7721c
ra-kesh <iamrakeshkumar@pm.me>
2024-04-17 14:00:00 +0530

    feat:redo-grid

--- app/(dashboard)/generate-image/_component/ImageGrid.tsx
+++ app/(dashboard)/generate-image/_component/ImageGrid.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 
 import { Button } from "@/components/ui/button"
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 const ImageGrid = ({
   images,
@@ -13,21 +14,23 @@ const ImageGrid = ({
   setSelectedImage: any
 }) => {
   return (
-    <div className="grid grid-cols-1 gap-4 rounded-xl bg-form lg:grid-cols-2 lg:p-3 xl:grid-cols-3 xl:p-4 4xl:rounded-3xl 4xl:p-10 ">
-      {images?.map((image: any) => (
-        <div className="group relative lg:col-span-1" key={image.id}>
-          <img src={image.url} alt="generated image" className="size-full rounded-xl" />
-          <div className="absolute left-0 top-0 flex size-full flex-col items-center justify-center opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-            <Button
-              className="w-2/3 bg-gradient-to-bl from-[#7f04e3] to-[#ff006b] 4xl:h-16 4xl:rounded-3xl 4xl:text-2xl"
-              onClick={() => setSelectedImage(image)}
-            >
-              View
-            </Button>
+    <ScrollArea className="rounded-xl bg-form lg:h-[40rem] lg:p-3 xl:h-[39rem] xl:p-4 2xl:h-[49rem] 2xl:p-6 4xl:h-[76rem] 4xl:rounded-3xl 4xl:p-10">
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2  xl:grid-cols-3">
+        {images?.map((image: any) => (
+          <div className="group relative lg:col-span-1" key={image.id}>
+            <img src={image.url} alt="generated image" className="size-full rounded-xl" />
+            <div className="absolute left-0 top-0 flex size-full flex-col items-center justify-center opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              <Button
+                className="w-2/3 bg-gradient-to-bl from-[#7f04e3] to-[#ff006b] 4xl:h-16 4xl:rounded-3xl 4xl:text-2xl"
+                onClick={() => setSelectedImage(image)}
+              >
+                View
+              </Button>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </ScrollArea>
   )
 }
 
