81031e333aa2d2bc432fec4ff8e58b4ec7c0ff1a
ra-kesh <iamrakeshkumar@pm.me>
2024-04-17 16:12:11 +0530

    feat:remove unnecessary

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,3 @@
 export default function Home() {
-  return (
-    <main>
-      <div>
-        <svg width="full" height="full" viewBox="0 0 2198 1524" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <g filter="url(#filter0_f_67_5995)">
-            <ellipse opacity="0.3" cx="1675.67" cy="400.599" rx="522.328" ry="400.599" fill="#3840FF" />
-            <ellipse cx="522.328" cy="567.599" rx="522.328" ry="400.599" fill="#A661FF" />
-            <ellipse cx="421.217" cy="511.391" rx="406.576" ry="311.637" fill="black" />
-            <ellipse cx="973.93" cy="504.842" rx="406.576" ry="311.637" fill="#EAAFFF" />
-            <ellipse cx="622.336" cy="801.197" rx="370.404" ry="283.803" fill="#FFD873" />
-            <ellipse cx="869.754" cy="684.402" rx="370.404" ry="283.803" fill="#FFD873" />
-            <ellipse cx="1627.2" cy="536.497" rx="450.707" ry="345.475" fill="#8960FF" fillOpacity="0.94" />
-            <ellipse cx="1828" cy="632" rx="299" ry="260" fill="#AF7EFF" fillOpacity="0.94" />
-            <ellipse cx="466.071" cy="536.497" rx="134.561" ry="69.3134" fill="#0029FF" />
-            <ellipse cx="496" cy="503.5" rx="164" ry="102.5" fill="#711AFF" />
-          </g>
-          {/* <defs>
-            <filter
-              id="filter0_f_67_5995"
-              x="-283.591"
-              y="-283.591"
-              width="2765.18"
-              height="1652.18"
-              filterUnits="userSpaceOnUse"
-              color-interpolation-filters="sRGB"
-            >
-              <feFlood flood-opacity="0" result="BackgroundImageFix" />
-              <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-              <feGaussianBlur stdDeviation="141.795" result="effect1_foregroundBlur_67_5995" />
-            </filter>
-          </defs> */}
-        </svg>
-      </div>
-    </main>
-  )
+  return <main></main>
 }
