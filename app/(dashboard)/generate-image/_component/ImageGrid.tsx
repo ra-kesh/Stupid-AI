9b0c97c83ff0c1d461379e28865bee5484d6415f
ra-kesh <iamrakeshkumar@pm.me>
2024-04-18 11:49:08 +0530

    feat:image-grid

--- app/(dashboard)/generate-image/_component/ImageGrid.tsx
+++ app/(dashboard)/generate-image/_component/ImageGrid.tsx
@@ -14,8 +14,8 @@ const ImageGrid = ({
   setSelectedImage: any
 }) => {
   return (
-    <ScrollArea className="rounded-xl bg-form lg:h-[40rem] lg:p-3 xl:h-[39rem] xl:p-4 2xl:h-[49rem] 2xl:p-6 4xl:h-[76rem] 4xl:rounded-3xl 4xl:p-10">
-      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2  xl:grid-cols-3">
+    <ScrollArea className="h-[25rem] rounded-xl bg-form sm:h-[30rem] md:h-[26rem] lg:h-[40rem] lg:p-3 xl:h-[39rem] xl:p-4 2xl:h-[49rem] 2xl:p-6 4xl:h-[76rem] 4xl:rounded-3xl 4xl:p-10">
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-2  xl:grid-cols-3">
         {images?.map((image: any) => (
           <div className="group relative lg:col-span-1" key={image.id}>
             <img src={image.url} alt="generated image" className="size-full rounded-xl" />
